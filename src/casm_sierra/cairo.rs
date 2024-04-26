cb4643ad781afb61c8fe72fd4ca1dac39e0e39db
Jelilat Anofiu <anofiujelailah@gmail.com>
2024-04-26 13:05:39 +0100

    add cairo execution trace

--- src/casm_sierra/cairo.rs
+++ src/casm_sierra/cairo.rs
@@ -1,4 +1,5 @@
 use anyhow::Context;
+use cairo_lang_casm::assembler::InstructionRepr;
 use cairo_lang_sierra::ProgramParser;
 use cairo_lang_sierra_to_casm::compiler::{compile, SierraToCasmConfig};
 use cairo_lang_sierra_to_casm::metadata::calc_metadata;
@@ -6,8 +7,21 @@ use std::collections::HashMap;
 use std::fs;
 
 pub type CasmSierraMapping = HashMap<u64, Vec<u64>>;
+#[derive(Debug)]
+pub struct CasmInstruction {
+    memory: String,
+    instruction_index: usize,
+    instruction_representation: Option<InstructionRepr>,
+}
+#[derive(Debug)]
+pub struct CasmSierraMappingInstruction {
+    casm_instructions: Vec<CasmInstruction>,
+    casm_sierra_mapping: CasmSierraMapping,
+}
 
-fn compile_sierra_to_casm(sierra_program: String) -> Result<CasmSierraMapping, anyhow::Error> {
+fn compile_sierra_to_casm(
+    sierra_program: String,
+) -> Result<CasmSierraMappingInstruction, anyhow::Error> {
     let program = ProgramParser::new()
         .parse(&sierra_program)
         .map_err(|_| anyhow::anyhow!("Failed to parse sierra program"))?;
@@ -23,6 +37,31 @@ fn compile_sierra_to_casm(sierra_program: String) -> Result<CasmSierraMapping, a
     )
     .with_context(|| "Compilation failed.")?;
 
+    let instructions = cairo_program.instructions;
+    let mut casm_instructions = Vec::new();
+    for (index, instruction) in instructions.iter().enumerate() {
+        let instruction_representation = instruction.assemble();
+        let mut first = true;
+        let encoded_instructions = instruction_representation.encode();
+        for encoded_instruction in encoded_instructions.iter() {
+            let hex_instruction = format!("0x{:x}", encoded_instruction);
+            if first {
+                casm_instructions.push(CasmInstruction {
+                    memory: hex_instruction,
+                    instruction_representation: Some(instruction.assemble()),
+                    instruction_index: index,
+                });
+                first = false; // Set first to false after the first iteration
+            } else {
+                casm_instructions.push(CasmInstruction {
+                    memory: hex_instruction,
+                    instruction_representation: None,
+                    instruction_index: index,
+                });
+            }
+        }
+    }
+
     let debug_info = cairo_program.debug_info;
     let sierra_statement_info = debug_info.sierra_statement_info;
 
@@ -36,7 +75,11 @@ fn compile_sierra_to_casm(sierra_program: String) -> Result<CasmSierraMapping, a
             .push(sierra_statement_index);
         sierra_statement_index += 1;
     }
-    Ok(casm_sierra_mapping)
+
+    Ok(CasmSierraMappingInstruction {
+        casm_instructions,
+        casm_sierra_mapping,
+    })
 }
 
 #[cfg(test)]
@@ -45,8 +88,7 @@ mod tests {
 
     #[test]
     fn test_compile_sierra_to_casm() {
-        let sierra_program = fs::read_to_string("<REPLACE_WITH_PATH_TO_SIERRA_PROGRAM>")
-            .expect("Could not read file!");
+        let sierra_program = fs::read_to_string("/fib.sierra").expect("Could not read file!");
 
         let casm_sierra_mapping =
             compile_sierra_to_casm(sierra_program).expect("Compilation failed");
---
+++ src/casm_sierra/cairo_contract.rs
@@ -0,0 +1,61 @@
+use std::fs;
+
+use anyhow::Context;
+use cairo_lang_starknet_classes::allowed_libfuncs::ListSelector;
+use cairo_lang_starknet_classes::casm_contract_class::CasmContractClass;
+use cairo_lang_starknet_classes::contract_class::{ContractClass, ContractEntryPoints};
+use cairo_lang_utils::bigint::BigUintAsHex;
+use clap::Parser;
+use serde::Deserialize;
+
+/// Same as `ContractClass` - but ignores `abi` in deserialization.
+/// Enables loading old contract classes.
+#[derive(Deserialize)]
+pub struct ContractClassIgnoreAbi {
+    pub sierra_program: Vec<BigUintAsHex>,
+    pub sierra_program_debug_info: Option<cairo_lang_sierra::debug_info::DebugInfo>,
+    pub contract_class_version: String,
+    pub entry_points_by_type: ContractEntryPoints,
+    pub _abi: Option<serde_json::Value>,
+}
+
+fn conpile_contract_sierra_to_casm() -> anyhow::Result<()> {
+    let list_selector = ListSelector::DefaultList;
+    let ContractClassIgnoreAbi {
+        sierra_program,
+        sierra_program_debug_info,
+        contract_class_version,
+        entry_points_by_type,
+        _abi,
+    } = serde_json::from_str(
+        &fs::read_to_string("/Users/jelilat/nethermind/hello_cairo/src/Storage.json")
+            .with_context(|| "Failed to read file.")?,
+    )
+    .with_context(|| "Deserialization failed.")?;
+    let contract_class = ContractClass {
+        sierra_program,
+        sierra_program_debug_info,
+        contract_class_version,
+        entry_points_by_type,
+        abi: None,
+    };
+    contract_class.validate_version_compatible(list_selector)?;
+    let casm_contract = CasmContractClass::from_contract_class(contract_class, false, 180000)
+        .with_context(|| "Compilation failed.")?;
+
+    println!("{:?}", casm_contract);
+    let res = serde_json::to_string_pretty(&casm_contract)
+        .with_context(|| "Casm contract Serialization failed.")?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_conpile_contract_sierra_to_casm() -> anyhow::Result<()> {
+        conpile_contract_sierra_to_casm()
+    }
+}
