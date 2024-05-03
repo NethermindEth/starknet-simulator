c44fef9d1f600d5da6fb59fb075486548cef1ff2
Jelilat Anofiu <anofiujelailah@gmail.com>
2024-05-03 10:59:29 +0100

    use temp_file

--- src/compiler/compile.rs
+++ src/compiler/compile.rs
@@ -1,21 +1,38 @@
+use std::io::Write;
+use tempfile::{tempdir, NamedTempFile};
+
 use crate::cairo_sierra::cairo::compile_cairo;
 use crate::cairo_sierra::cairo_helper::FullProgram;
 use crate::casm_sierra::cairo::{compile_sierra_to_casm, SierraCompile};
-use crate::compiler::helper::{trace_error, CompilationResultType};
 
 use anyhow::{Context, Result};
-use std::fs;
 
 pub struct CompilationResult {
     pub cairo_sierra: FullProgram,
     pub casm_sierra: SierraCompile,
 }
 
-pub fn compile(cairo_path: String, sierra_path: String) -> Result<CompilationResult> {
-    let full_program = compile_cairo(cairo_path.clone());
-    let cairo_sierra = full_program.unwrap();
-    fs::write(&sierra_path, format!("{}", cairo_sierra.program))
-        .context("Failed to write output to {}")?;
+pub fn compile(code: &str, file_name: &str) -> Result<CompilationResult> {
+    // Create a temporary directory
+    let dir = tempdir()?;
+
+    let cairo_file_path = dir.path().join(format!("{}.cairo", file_name));
+
+    // Create and write to the file
+    let mut cairo_temp_file = NamedTempFile::new_in(dir.path())?;
+    cairo_temp_file.write_all(code.as_bytes())?;
+    cairo_temp_file.persist(&cairo_file_path)?;
+    let cairo_path = cairo_file_path.to_str().unwrap().to_string();
+
+    let full_program = compile_cairo(cairo_path);
+    let cairo_sierra = full_program?;
+
+    let sierra_file_path = cairo_file_path.with_extension("sierra");
+
+    let mut sierra_temp_file = NamedTempFile::new_in(dir.path())?;
+    sierra_temp_file.write_all(format!("{}", cairo_sierra.program).as_bytes())?;
+    sierra_temp_file.persist(&sierra_file_path)?;
+    let sierra_path = sierra_file_path.to_str().unwrap().to_string();
 
     let casm_program = compile_sierra_to_casm(sierra_path);
     let casm_program = casm_program.with_context(|| "Failed to compile CASM program")?; // Added with_context for casm_program
@@ -25,10 +42,26 @@ pub fn compile(cairo_path: String, sierra_path: String) -> Result<CompilationRes
     })
 }
 
-pub fn main() {
-    let pc_error = 8; // REPLACE_WITH_PROGRAM_COUNTER_ERROR
-    let cairo_path = "Cairo_file_path.cairo".to_string();
-    let sierra_path = "Sierra_file_path.sierra".to_string();
-    let full_program = compile(cairo_path, sierra_path).unwrap();
-    trace_error(pc_error, CompilationResultType::General(full_program));
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_compile() {
+        let code = r#"use core::felt252;
+
+        fn main() -> felt252 {
+            let n = 10;
+            let result = fib(1, 1, n);
+            result
+        }
+        
+        fn fib(a: felt252, b: felt252, n: felt252) -> felt252 {
+            match n {
+                0 => a,
+                _ => fib(b, a + b, n - 1),
+            }
+        }
+        "#;
+        let file_name = "fib";
+        super::compile(code, file_name).unwrap();
+    }
 }
--- src/compiler/compile_contract.rs
+++ src/compiler/compile_contract.rs
@@ -1,28 +1,40 @@
+use std::io::Write;
+use tempfile::{tempdir, NamedTempFile};
+
 use crate::cairo_sierra::cairo_contract::compile_contract_cairo_to_sierra;
 use crate::cairo_sierra::compile::FullProgram;
-use crate::casm_sierra::cairo::CasmSierraMappingInstruction;
 use crate::casm_sierra::cairo_contract::conpile_contract_sierra_to_casm;
 use crate::casm_sierra::cairo_contract_helper::SierraContractCompile;
-use crate::compiler::helper::{trace_error, CompilationResultType};
 
 use anyhow::{Context, Result};
-use std::fs;
 
 pub struct ContractCompilationResult {
     pub cairo_sierra: FullProgram,
     pub casm_sierra: SierraContractCompile,
 }
 
-pub fn compile_contract(
-    cairo_path: String,
-    sierra_path: String,
-) -> Result<ContractCompilationResult> {
-    // Changed return type to Result from anyhow
+pub fn compile_contract(code: &str, file_name: &str) -> Result<ContractCompilationResult> {
+    // Create a temporary directory
+    let dir = tempdir()?;
+
+    let cairo_file_path = dir.path().join(format!("{}.cairo", file_name));
+
+    // Create and write to the file
+    let mut cairo_temp_file = NamedTempFile::new_in(dir.path())?;
+    cairo_temp_file.write_all(code.as_bytes())?;
+    cairo_temp_file.persist(&cairo_file_path)?;
+    let cairo_path = cairo_file_path.to_str().unwrap().to_string();
+
     let full_program = compile_contract_cairo_to_sierra(cairo_path.clone());
     let cairo_sierra = full_program.unwrap();
     let program = serde_json::to_string_pretty(&cairo_sierra.contract_class).unwrap();
-    fs::write(&sierra_path, program)
-        .with_context(|| format!("Failed to write output to {}", sierra_path))?; // Added format! for context
+
+    let sierra_file_path = cairo_file_path.with_extension("sierra");
+
+    let mut sierra_temp_file = NamedTempFile::new_in(dir.path())?;
+    sierra_temp_file.write_all(program.as_bytes())?;
+    sierra_temp_file.persist(&sierra_file_path)?;
+    let sierra_path = sierra_file_path.to_str().unwrap().to_string();
 
     let casm_program = conpile_contract_sierra_to_casm(sierra_path);
     let casm_program = casm_program.with_context(|| "Failed to compile CASM program")?; // Added with_context for casm_program
@@ -32,10 +44,37 @@ pub fn compile_contract(
     })
 }
 
-pub fn main() {
-    let pc_error = 67; // REPLACE_WITH_PROGRAM_COUNTER_ERROR
-    let cairo_path = "Cairo_file_path.cairo".to_string();
-    let sierra_path = "Sierra_file_path.json".to_string();
-    let full_program = compile_contract(cairo_path, sierra_path).unwrap();
-    trace_error(pc_error, CompilationResultType::Contract(full_program));
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_compile() {
+        let code = r#"#[starknet::interface]
+        pub trait IHelloStarknet<TContractState> {
+            fn increase_balance(ref self: TContractState, amount: felt252);
+            fn get_balance(self: @TContractState) -> felt252;
+        }
+        
+        #[starknet::contract]
+        mod HelloStarknet {
+            #[storage]
+            struct Storage {
+                balance: felt252, 
+            }
+        
+            #[abi(embed_v0)]
+            impl HelloStarknetImpl of super::IHelloStarknet<ContractState> {
+                fn increase_balance(ref self: ContractState, amount: felt252) {
+                    assert(amount != 0, 'Amount cannot be 0');
+                    self.balance.write(self.balance.read() + amount);
+                }
+        
+                fn get_balance(self: @ContractState) -> felt252 {
+                    self.balance.read()
+                }
+            }
+        }
+        "#;
+        let file_name = "Balance";
+        super::compile_contract(code, file_name).unwrap();
+    }
 }
